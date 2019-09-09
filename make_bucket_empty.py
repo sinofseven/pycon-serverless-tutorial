72a97088beec72924cb5fe72cc62e5e8a1236880
yuta-natsume <natsume.yuta@classmethod.jp>
2019-09-09 18:36:01 +0900

    add

---
+++ make_bucket_empty.py
@@ -0,0 +1,29 @@
+import boto3
+import os
+
+
+def get_outputs(stack_name):
+    cfn = boto3.client('cloudformation')
+    resp = cfn.describe_stacks(StackName=stack_name)
+    if 'Stacks' not in resp or len(resp['Stacks']) == 0:
+        return {}
+    return {
+        x['OutputKey']: x['OutputValue']
+        for x in resp['Stacks'][0].get('Outputs', [])
+    }
+
+
+def to_empty(bucket_name):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    for file in bucket.objects.all():
+        file.delete()
+
+
+stack_name = os.environ['STACK_NAME']
+
+outpus = get_outputs(stack_name)
+
+bucket_name = outpus['DataBucketName']
+print(bucket_name)
+to_empty(bucket_name)
